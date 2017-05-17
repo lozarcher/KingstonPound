41e7084d580a70c8a88c1066a3882f22b22a7b50
Georgiana <Geor23@users.noreply.github.com>
2017-05-17 15:37:30 +0100

    set up Config and Colors packages for import and fixed expo error popping up

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const config={
+    FLAVOUR: 'release',
+    STAGE_SERVER: 'https://bristol-stage.community-currency.org/cyclos/api/',
+    DEV_SERVER: 'https://bristol.cyclos.org/bristolpoundsandbox03/api/',
+    BASE_URL: 'https://bristol-stage.community-currency.org/cyclos/bristolpound/api/',
+    APP_CITY: 'Bristol',
+    APP_WEBSITE: 'http://bristolpound.org'
+}
+
+export default config
