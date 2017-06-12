e26df8816923d12c1beb264ba7c739b3037eba33
Georgiana Ginghina <Geor23@users.noreply.github.com>
2017-06-12 13:48:30 +0100

    added text to pay config no (#991)
    * added text to pay config no
    
    * change reference
    
    * updated submodule reference

--- config/config.js
+++ config/config.js
@@ -4,7 +4,8 @@ const config={
     DEV_SERVER: 'https://bristol.cyclos.org/bristolpoundsandbox03/api/',
     BASE_URL: 'https://bristol-stage.community-currency.org/cyclos/bristolpound/api/',
     APP_CITY: 'Bristol',
-    APP_WEBSITE: 'http://bristolpound.org'
+    APP_WEBSITE: 'http://bristolpound.org',
+    TXT2PAY_NO: '+44 7441 906260'
 }
 
 export default config
