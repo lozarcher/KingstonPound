13f6deec94179c0ae69a3440b1579f8e6f0be88c
Martin Rüegg <martin.rueegg@metaworx.ch>
2017-07-28 16:41:18 +0100

    update config (#1060)
    * update config
    
    * fix typo
    
    * use default config
    
    * bugfix
    
    * use different currency names

--- config/config.js
+++ config/config.js
@@ -1,36 +1,49 @@
-const config={
-    FLAVOUR: 'release',
-	STAGE_CONFIG: {
-		  host: 'api.bristolpound.org'
-		, cyclosPrefix: ''
-		, network: ''
-		, wsPrefix: ''
-	},
-    DEV_CONFIG: {
-		  host: 'bristol.cyclos.org'
-		, cyclosPrefix: ""
-		, network: 'bristolpoundsandbox03'
-	},
-    PROD_CONFIG: {
-		  host: 'api.bristolpound.org'
-		, cyclosPrefix: ''
-		, network: ''
-		, wsPrefix: ''
-	},
+import { normaliseConfigurations } from '../CityPoundSourceCode/src/util/config'
+
+// export this to see the original selection
+export const flavour = 'staging'
+
+// the base configuration
+export const default_config={
     APP_CITY: 'Bristol',
+    APP_CURRENCY: 'Bristol Default',
     APP_WEBSITE: 'https://bristolpound.org',
     TXT2PAY_NO: '+44 7441 906260',
-    ALLOW_LOGIN: true
-    , CASH_POINT_1: 'This is a cash point, where you can swap Sterling for Bristol Pounds.'
-    , CASH_POINT_2: 'This is a cash point, where you can swap Sterling for Bristol Pounds.'
+    CYCLOS: {
+        host: 'bristol-stage.community-currency.org',
+        cyclosPrefix: 'cyclos',
+        network: 'bristolpound',
+        wsPrefix: ''
+    },
+    DIRECTORY: {
+        host: 'api.bristolpound.org',
+        cyclosPrefix: '',
+        network: '',
+        wsPrefix: ''
+    },
+    ALLOW_LOGIN: true,
+    DEFAULT_COORDINATES: { latitude: 51.454513, longitude:  -2.58791 },
+    CASH_POINT_1: 'This is a cash point, where you can swap Sterling for Bristol Pounds.',
+    CASH_POINT_2: 'This is a cash point, where you can swap Sterling for Bristol Pounds.'
 }
 
-// ToDo: needs to be replaced at some point with a CyclosHost object, similar to the config in DirectoryAPI
-config.STAGE_SERVER	= 'https://bristol-stage.community-currency.org/cyclos/bristolpound/api/'
-config.DEV_SERVER 	= 'https://'+config.DEV_CONFIG.host		+'/'+config.STAGE_CONFIG.network+'/api/'
-config.BASE_URL	= 'https://bristol-stage.community-currency.org/cyclos/bristolpound/api/'
+// customisations for individual flavours are added here
+export const configurations={
+    staging: {
+        APP_CURRENCY: 'Bristol Staging',
+    },
+    development: {
+        APP_CURRENCY: 'Bristol Devel',
+    },
+    release: {
+        APP_CURRENCY: 'Bristol Pound',
+        TXT2PAY_NO: '+44 7441 900 333​',
+        CYCLOS: {
+              host: 'bristol.community-currency.org',
+        }
+	}
+}
 
-// ToDo: needs to also be able to use DEV_CONFIG
-config.CURRENT_CONFIG = (config.FLAVOUR == 'release' ? config.PROD_CONFIG : config.STAGE_CONFIG)
+const config = normaliseConfigurations(configurations, flavour, default_config)
 
 export default config
