e46d67d20e5e1d1a19be059e3e14ff80db98e4d2
Chris <cmeehan@scottlogic.com>
2017-09-25 15:53:51 +0100

    cash only text in config

--- config/config.js
+++ config/config.js
@@ -22,6 +22,7 @@ export const default_config={
         cyclosPrefix: '',
         network: '',
         wsPrefix: '',
+        apiVersion: '0.3'
     },
     ALLOW_LOGIN: true,
     DEFAULT_COORDINATES: { latitude: 51.454513, longitude:  -2.58791 },
@@ -50,5 +51,7 @@ export const configurations={
 }
 
 export const privacyPolicy = `<div><p>By using the Bristol Pound mobile application, you are giving your consent to the terms of this privacy policy and to the collection, processing and storage of your personal information for the purposes set forth herein. If you do not agree to this privacy policy, we ask that you desist from using the application as a way of opting out.</p><p>We reserve the right, at our discretion, to change this privacy policy at any time. Such change will be effective 30 days following posting of the revised privacy policy, and your continued use of the Services thereafter means that you accept those changes.</p><p>The Bristol Pound mobile application collects information for the purposes of improving the services we offer, and for effectively promoting services to you. The application collects data as aggregated, non-personally-identifiable information. This includes transaction dates and times, user types, approximate geolocations, usage patterns and data about the functioning of the application. Information about personal payments is not stored, nor is personally identifiable information about your transaction history or personal shopping habits. All payment data is pulled from and stored on the online banking server run by the Bristol Credit Union (BCU), and is covered under your separate data protection agreement directly with the BCU.</p><p>Your rights to access data stored about you remain as per our data protection policy at: <a href="https://bristolpound.org/data-protection-pledge" target="_blank">https://bristolpound.org/data-protection-pledge</a></p></div>`
+
+export const cashOnlyText = 'Cash Only Business'
 
 export default config = {}
